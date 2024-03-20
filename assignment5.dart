4a7e516f11cb9833f7269132258e4fa5c62e72f6
Asher Ayub <asherayub80@gmail.com>
2024-03-20 22:25:14 +0500

    Update assignment5.dart

--- assignment5.dart
+++ assignment5.dart
@@ -8,20 +8,20 @@ void main() {
   studentinfo('');
   populationinfo('');
 }
-// Q.1: Write a Dart program to create a list of your favorite fruits and print them in alphabetical order.
+//? Q.1: Write a Dart program to create a list of your favorite fruits and print them in alphabetical order.
 
 void listoffruit(String fruits) {
   List fruits = ["Mango", "Apple", "Bannana", "Orange"];
   fruits.sort();
   print(fruits);
 }
-// Q.2: Create a list of even numbers from 1 to 20 and print them.
+//? Q.2: Create a list of even numbers from 1 to 20 and print them.
 
 void listofevennum(int evennum) {
   List evennum = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20];
   print(evennum);
 }
-// Q.3: Write a Dart program to remove all duplicate elements from a given list of numbers.
+//? Q.3: Write a Dart program to remove all duplicate elements from a given list of numbers.
 
 void removeduplicate(List<int> numbers) {
   Set<int> uniqueSet = {};
@@ -30,7 +30,7 @@ void removeduplicate(List<int> numbers) {
   });
   print("List without Duplicate:$numbers");
 }
-// Q.4: Write a Dart program to check if a given list is empty or not.
+//? Q.4: Write a Dart program to check if a given list is empty or not.
 
 void checkList(var myList) {
   List myList = [];
@@ -41,18 +41,22 @@ void checkList(var myList) {
   }
 }
 
-// Q.5: Create two lists of numbers and merge them into a single list. Print the merged list.
+// ? Q.5: Create two lists of numbers and merge them into a single list. Print the merged list.
 
 void mergelist(int lists) {
   List listone = [1, 2, 3, 4, 5, 6];
   List listtwo = [7, 8, 9, 10];
   print(listone + listtwo);
 }
+// ? Q.6: Write a Dart program to create a map representing a student's information including name, age, and grade. Print out all the information stored in the map.
+
 
 void studentinfo(dynamic info) {
   Map details = {'name': 'Asher', 'Age': '19', 'Grade': 'A'};
   print(details);
 }
+// ? Q.7: Create a map representing the population of different cities. Write a Dart program to find and print the city with the highest population.
+
 
 void populationinfo(dynamic popinfo) {
   Map<String, int>? populationdetail = {
